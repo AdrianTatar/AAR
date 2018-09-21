e4a5fd5ba0cbcab11355710410368233741b2104
Pop John <popioanturda@gmail.com>
2018-09-21 14:20:38 +0200

    Navigation Bar -Menu UI

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -8,16 +8,24 @@ import { TranslateService } from '@ngx-translate/core';
 })
 export class NavbarComponent implements OnInit {
   language = '';
+  image = '';
 
   constructor(private translate: TranslateService) {
     translate.setDefaultLang('en');
     this.language = 'EN';
+    this.image = '../../assets/resources/united-kingdom.png';
   }
 
   switchLanguage(language: string) {
     this.translate.use(language);
     this.language = language.toUpperCase();
+
+    if (this.language === 'EN') {
+      this.image = '../../assets/resources/united-kingdom.png';
+    } else {
+      this.image = '../../assets/resources/germany.png';
+    }
   }
 
-  ngOnInit() { }
+  ngOnInit() {}
 }
