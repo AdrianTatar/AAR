8d335d28dc5cdc6b6066755ce1422f3a03a1e85c
Pop John <popioanturda@gmail.com>
2018-10-05 11:00:29 +0200

    Logs Base Class Change

--- src/app/logs/logs.component.ts
+++ src/app/logs/logs.component.ts
@@ -1,13 +1,6 @@
 import { Component, OnInit, ViewChild, HostListener } from '@angular/core';
-<<<<<<< HEAD
-import { MatPaginator, MatTableDataSource } from '@angular/material';
-import { UserAction } from '../shared/models/user.actions';
-import { Router } from '@angular/router';
-import { UserService } from '../user.service';
-=======
 import { MatPaginator, MatTableDataSource, MatDialog } from '@angular/material';
-import { UserAction } from '../shared/models/UserActions';
->>>>>>> 679affa8340a35c0d5507d0cd1a89e05c4af6ff8
+import { UserAction } from '../shared/models/user.actions';
 
 @Component({
   selector: 'app-logs',
@@ -17,20 +10,12 @@ import { UserAction } from '../shared/models/UserActions';
 export class LogsComponent implements OnInit {
 
   displayedColumns: string[] = ['position', 'timestamp', 'uid', 'action'];
-<<<<<<< HEAD
-  dataSource = new MatTableDataSource<UserAction>();
-=======
   dataSource = new MatTableDataSource<UserAction>(USER_ACTIONS);
   filteredDataSource = new MatTableDataSource<UserAction>(USER_ACTIONS);
 
->>>>>>> 679affa8340a35c0d5507d0cd1a89e05c4af6ff8
   @ViewChild(MatPaginator) paginator: MatPaginator;
   selectedRow = -1;
-  userActions: UserAction[];
 
-<<<<<<< HEAD
-  constructor(private userService: UserService) {
-=======
   logIdSearch = false;
   timestampSearch = false;
   userIdSearch = false;
@@ -49,7 +34,7 @@ export class LogsComponent implements OnInit {
     this.filteredDataSource.data = this.dataSource.data;
 
     this.filteredDataSource.data = (this.logIdSearchQuery) ?
-      this.filteredDataSource.data.filter(p => p.position === this.logIdSearchQuery)
+      this.filteredDataSource.data.filter(p => p.id === this.logIdSearchQuery)
       : this.filteredDataSource.data;
 
     this.filteredDataSource.data = (this.timestampSearchQuery) ?
@@ -58,7 +43,7 @@ export class LogsComponent implements OnInit {
       : this.filteredDataSource.data;
 
     this.filteredDataSource.data = (this.userIdSearchQuery) ?
-      this.filteredDataSource.data.filter(p => p.uid.toLocaleLowerCase()
+      this.filteredDataSource.data.filter(p => p.userid.toLocaleLowerCase()
         .includes(this.userIdSearchQuery.toLocaleLowerCase()))
       : this.filteredDataSource.data;
 
@@ -82,24 +67,11 @@ export class LogsComponent implements OnInit {
       this.actionSearchQuery = '';
     }
     this.filter();
->>>>>>> 679affa8340a35c0d5507d0cd1a89e05c4af6ff8
   }
 
-  testCreate() {
-    this.userService.createUser(USER_ACTIONS[0]);
-  }
-
-  async ngOnInit() {
+  ngOnInit() {
     this.dataSource.paginator = this.paginator;
-<<<<<<< HEAD
-    await this.userService.getUsers().subscribe( data => {
-      this.userActions = data;
-      // console.log("data: "+data);
-      this.dataSource.data = this.userActions;
-    });
-=======
     this.selectedRow = 0;
->>>>>>> 679affa8340a35c0d5507d0cd1a89e05c4af6ff8
   }
 
   @HostListener('document:keydown', ['$event']) handleKeyboardEvent(event: KeyboardEvent) {
@@ -119,99 +91,8 @@ export class LogsComponent implements OnInit {
 }
 
 const USER_ACTIONS: UserAction[] = [
-<<<<<<< HEAD
-  { id: null, timestamp: new Date(), userid: 'vosi312', action: 'delete chestie' },
-  // { position: 2, timestamp: new Date(), uid: 'luis312', action: 'review chestie' },
-  // { position: 3, timestamp: new Date(), uid: 'ieto312', action: 'update chestie' },
-  // { position: 4, timestamp: new Date(), uid: 'toie312', action: 'add chestie' }
-=======
-  { position: 1, timestamp: new Date(), uid: 'vosi312', action: 'delete chestie' },
-  { position: 2, timestamp: new Date(), uid: 'luis312', action: 'review chestie' },
-  { position: 3, timestamp: new Date(), uid: 'ieto312', action: 'update chestie' },
-  { position: 4, timestamp: new Date(), uid: 'toie312', action: 'add chestie' },
-  { position: 5, timestamp: new Date(), uid: 'ieto312', action: 'update chestie' },
-  { position: 6, timestamp: new Date(), uid: 'ieto312', action: 'update chestie' },
-  { position: 7, timestamp: new Date(), uid: 'ieto312', action: 'update chestie' },
-  { position: 8, timestamp: new Date(), uid: 'ieto312', action: 'update chestie' },
-  { position: 9, timestamp: new Date(), uid: 'ieto312', action: 'update chestie' },
-  { position: 1, timestamp: new Date(), uid: 'ieto312', action: 'update chestie' },
-  { position: 2, timestamp: new Date(), uid: 'ieto312', action: 'update chestie' },
-  { position: 3, timestamp: new Date(), uid: 'vosi312', action: 'delete chestie' },
-  { position: 4, timestamp: new Date(), uid: 'luis312', action: 'review chestie' },
-  { position: 5, timestamp: new Date(), uid: 'ieto312', action: 'update chestie' },
-  { position: 6, timestamp: new Date(), uid: 'toie312', action: 'add chestie' },
-  { position: 7, timestamp: new Date(), uid: 'ieto312', action: 'update chestie' },
-  { position: 8, timestamp: new Date(), uid: 'ieto312', action: 'update chestie' },
-  { position: 9, timestamp: new Date(), uid: 'ieto312', action: 'update chestie' },
-  { position: 1, timestamp: new Date(), uid: 'ieto312', action: 'update chestie' },
-  { position: 2, timestamp: new Date(), uid: 'ieto312', action: 'update chestie' },
-  { position: 3, timestamp: new Date(), uid: 'ieto312', action: 'update chestie' },
-  { position: 4, timestamp: new Date(), uid: 'ieto312', action: 'update chestie' },
-  { position: 5, timestamp: new Date(), uid: 'vosi312', action: 'delete chestie' },
-  { position: 6, timestamp: new Date(), uid: 'luis312', action: 'review chestie' },
-  { position: 7, timestamp: new Date(), uid: 'ieto312', action: 'update chestie' },
-  { position: 8, timestamp: new Date(), uid: 'toie312', action: 'add chestie' },
-  { position: 9, timestamp: new Date(), uid: 'ieto312', action: 'update chestie' },
-  { position: 1, timestamp: new Date(), uid: 'ieto312', action: 'update chestie' },
-  { position: 2, timestamp: new Date(), uid: 'ieto312', action: 'update chestie' },
-  { position: 3, timestamp: new Date(), uid: 'ieto312', action: 'update chestie' },
-  { position: 4, timestamp: new Date(), uid: 'ieto312', action: 'update chestie' },
-  { position: 5, timestamp: new Date(), uid: 'ieto312', action: 'update chestie' },
-  { position: 6, timestamp: new Date(), uid: 'ieto312', action: 'update chestie' },
-  { position: 7, timestamp: new Date(), uid: 'vosi312', action: 'delete chestie' },
-  { position: 8, timestamp: new Date(), uid: 'luis312', action: 'review chestie' },
-  { position: 9, timestamp: new Date(), uid: 'ieto312', action: 'update chestie' },
-  { position: 1, timestamp: new Date(), uid: 'toie312', action: 'add chestie' },
-  { position: 2, timestamp: new Date(), uid: 'ieto312', action: 'update chestie' },
-  { position: 3, timestamp: new Date(), uid: 'ieto312', action: 'update chestie' },
-  { position: 4, timestamp: new Date(), uid: 'ieto312', action: 'update chestie' },
-  { position: 5, timestamp: new Date(), uid: 'ieto312', action: 'update chestie' },
-  { position: 6, timestamp: new Date(), uid: 'ieto312', action: 'update chestie' },
-  { position: 7, timestamp: new Date(), uid: 'ieto312', action: 'update chestie' },
-  { position: 8, timestamp: new Date(), uid: 'ieto312', action: 'update chestie' },
-  { position: 9, timestamp: new Date(), uid: 'vosi312', action: 'delete chestie' },
-  { position: 1, timestamp: new Date(), uid: 'luis312', action: 'review chestie' },
-  { position: 2, timestamp: new Date(), uid: 'ieto312', action: 'update chestie' },
-  { position: 3, timestamp: new Date(), uid: 'toie312', action: 'add chestie' },
-  { position: 4, timestamp: new Date(), uid: 'ieto312', action: 'update chestie' },
-  { position: 5, timestamp: new Date(), uid: 'ieto312', action: 'update chestie' },
-  { position: 6, timestamp: new Date(), uid: 'ieto312', action: 'update chestie' },
-  { position: 7, timestamp: new Date(), uid: 'ieto312', action: 'update chestie' },
-  { position: 8, timestamp: new Date(), uid: 'ieto312', action: 'update chestie' },
-  { position: 9, timestamp: new Date(), uid: 'ieto312', action: 'update chestie' },
-  { position: 1, timestamp: new Date(), uid: 'ieto312', action: 'update chestie' },
-  { position: 2, timestamp: new Date(), uid: 'vosi312', action: 'delete chestie' },
-  { position: 3, timestamp: new Date(), uid: 'luis312', action: 'review chestie' },
-  { position: 4, timestamp: new Date(), uid: 'ieto312', action: 'update chestie' },
-  { position: 5, timestamp: new Date(), uid: 'toie312', action: 'add chestie' },
-  { position: 6, timestamp: new Date(), uid: 'ieto312', action: 'update chestie' },
-  { position: 7, timestamp: new Date(), uid: 'ieto312', action: 'update chestie' },
-  { position: 8, timestamp: new Date(), uid: 'ieto312', action: 'update chestie' },
-  { position: 9, timestamp: new Date(), uid: 'ieto312', action: 'update chestie' },
-  { position: 1, timestamp: new Date(), uid: 'ieto312', action: 'update chestie' },
-  { position: 2, timestamp: new Date(), uid: 'ieto312', action: 'update chestie' },
-  { position: 3, timestamp: new Date(), uid: 'ieto312', action: 'update chestie' },
-  { position: 4, timestamp: new Date(), uid: 'vosi312', action: 'delete chestie' },
-  { position: 5, timestamp: new Date(), uid: 'luis312', action: 'review chestie' },
-  { position: 6, timestamp: new Date(), uid: 'ieto312', action: 'update chestie' },
-  { position: 7, timestamp: new Date(), uid: 'toie312', action: 'add chestie' },
-  { position: 8, timestamp: new Date(), uid: 'ieto312', action: 'update chestie' },
-  { position: 9, timestamp: new Date(), uid: 'ieto312', action: 'update chestie' },
-  { position: 1, timestamp: new Date(), uid: 'ieto312', action: 'update chestie' },
-  { position: 2, timestamp: new Date(), uid: 'ieto312', action: 'update chestie' },
-  { position: 3, timestamp: new Date(), uid: 'ieto312', action: 'update chestie' },
-  { position: 4, timestamp: new Date(), uid: 'ieto312', action: 'update chestie' },
-  { position: 5, timestamp: new Date(), uid: 'ieto312', action: 'update chestie' },
-  { position: 6, timestamp: new Date(), uid: 'vosi312', action: 'delete chestie' },
-  { position: 7, timestamp: new Date(), uid: 'luis312', action: 'review chestie' },
-  { position: 8, timestamp: new Date(), uid: 'ieto312', action: 'update chestie' },
-  { position: 9, timestamp: new Date(), uid: 'toie312', action: 'add chestie' },
-  { position: 1, timestamp: new Date(), uid: 'ieto312', action: 'update chestie' },
-  { position: 2, timestamp: new Date(), uid: 'ieto312', action: 'update chestie' },
-  { position: 3, timestamp: new Date(), uid: 'ieto312', action: 'update chestie' },
-  { position: 4, timestamp: new Date(), uid: 'ieto312', action: 'update chestie' },
-  { position: 5, timestamp: new Date(), uid: 'ieto312', action: 'update chestie' },
-  { position: 6, timestamp: new Date(), uid: 'ieto312', action: 'update chestie' },
-  { position: 7, timestamp: new Date(), uid: 'ieto312', action: 'update chestie' }
->>>>>>> 679affa8340a35c0d5507d0cd1a89e05c4af6ff8
+  { id: 1, timestamp: new Date(), userid: 'vosi312', action: 'delete chestie' },
+  { id: 2, timestamp: new Date(), userid: 'luis312', action: 'review chestie' },
+  { id: 3, timestamp: new Date(), userid: 'ieto312', action: 'update chestie' },
+  { id: 4, timestamp: new Date(), userid: 'toie312', action: 'add chestie' }
 ];
